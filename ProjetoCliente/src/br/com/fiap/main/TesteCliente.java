5aad4b2526edec4ba01d5ccb59498e79e74f0740
Henrique Furtado <63440926+HenriqueFurtado-Dev@users.noreply.github.com>
2023-08-28 12:02:21 -0300

    Add files via upload

---
+++ ProjetoCliente/src/br/com/fiap/main/TesteCliente.java
@@ -0,0 +1,12 @@
+package br.com.fiap.main;
+
+public class TesteCliente {
+
+	public static void main(String[] args) {
+		// Instanciar objeto
+		Cliente objetoCliente = new Cliente();
+		// NomeArquivo nomeObjeto = new NomeArquivo();
+		
+	}
+
+}
